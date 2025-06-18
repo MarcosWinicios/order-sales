b1fa81d9ff91ce7d9416c46356c3c592ee599d78
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2025-06-18 09:33:09 -0300

    feat(repository): Add order repository class

---
+++ src/main/java/com/studies/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.studies.repository;
+
+import com.studies.model.Order;
+
+public class OrderRepository {
+
+    public void  save(Order order){
+        System.out.println("Saving order: " + order.getValue() + " for client: " + order.getClient().getName());
+    }
+}
