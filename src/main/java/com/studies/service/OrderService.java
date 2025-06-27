96f448ec943fed0a79204d7b03f9317fe6792f31
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2025-06-27 01:30:37 -0300

    feat(test): Add test to verify if email and sms notification method were called

--- src/main/java/com/studies/service/OrderService.java
+++ src/main/java/com/studies/service/OrderService.java
@@ -1,19 +1,27 @@
 package com.studies.service;
 
+import com.studies.email.EmailNotifier;
 import com.studies.model.Order;
 import com.studies.repository.OrderRepository;
+import com.studies.sms.SmsNotifier;
 
 public class OrderService {
     
     private OrderRepository orderRepository;
+    private EmailNotifier emailNotifier;
+    private SmsNotifier smsNotifier;
 
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(OrderRepository orderRepository, EmailNotifier emailNotifier, SmsNotifier smsNotifier) {
         this.orderRepository = orderRepository;
+        this.emailNotifier = emailNotifier;
+        this.smsNotifier = smsNotifier;
     }
     
     public double launch(Order order) {
 
         orderRepository.save(order);
+        emailNotifier.send(order);
+        smsNotifier.send(order);
 
         return order.getValue() * 0.1;
     }
--- src/test/java/com/studies/service/OrderServiceTest.java
+++ src/test/java/com/studies/service/OrderServiceTest.java
@@ -1,8 +1,10 @@
 package com.studies.service;
 
+import com.studies.email.EmailNotifier;
 import com.studies.model.Order;
 import com.studies.model.builder.OrderBuilder;
 import com.studies.repository.OrderRepository;
+import com.studies.sms.SmsNotifier;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -18,33 +20,47 @@ public class OrderServiceTest {
     @Mock
     private OrderRepository orderRepository;
 
+    @Mock
+    private EmailNotifier emailNotifier;
+
+    @Mock
+    private SmsNotifier smsNotifier;
+
+    private Order order;
+
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
+        orderService = new OrderService(orderRepository, emailNotifier, smsNotifier);
 
-        orderService = new OrderService(orderRepository);
-    }
-
-    @Test
-    public void mustCalculateTax() throws Exception{
-        Order order =  new OrderBuilder()
+        order =  new OrderBuilder()
                 .withValue(100.0)
                 .to("João", "joao@joao.com", "9999-0000")
                 .build();
 
+    }
+
+    @Test
+    public void mustCalculateTax() throws Exception{
         double tax = orderService.launch(order);
         assertEquals(10.0, tax, 0.0001);
     }
 
     @Test
     public void mustSaveOrderInDatabase() throws Exception{
-        Order order =  new OrderBuilder()
-                .withValue(100.0)
-                .to("João", "joao@joao.com", "9999-0000")
-                .build();
-
         orderService.launch(order);
-
         Mockito.verify(orderRepository).save(order);
+    }
+
+    @Test
+    public void mustNotifyByemail() throws Exception{
+        orderService.launch(order);
+        Mockito.verify(emailNotifier).send(order);
+    }
+
+    @Test
+    public void mustNotifyBySms() throws Exception {
+        orderService.launch(order);
+        Mockito.verify(smsNotifier).send(order);
     }
 }
